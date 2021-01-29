25d46a406d26ee93440f24f7e1dba3a75706b67e
SHughesCoe <shughes@coe.edu>
2021-01-29 11:02:15 -0600

    After Class 1/29

--- app/src/main/java/edu/coe/hughes/guistarter/MainActivity.kt
+++ app/src/main/java/edu/coe/hughes/guistarter/MainActivity.kt
@@ -2,10 +2,23 @@ package edu.coe.hughes.guistarter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun greet(v: View){
+        Toast.makeText(this, R.string.msg, Toast.LENGTH_LONG).show();
+    }
+
+    fun changeTxt(v:View){
+        var g = this.findViewById<TextView>(R.id.tvResponse);
+        g.setText("I've changed....");
     }
 }
